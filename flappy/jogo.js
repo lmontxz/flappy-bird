93e087df405ea18d960d21f0055df25a805a25ef
lmontxz <118456918+lmontxz@users.noreply.github.com>
2022-12-15 12:10:15 -0300

    Update jogo.js

--- flappy/jogo.js
+++ flappy/jogo.js
@@ -22,8 +22,62 @@ const flappyBird = {
         }
 }
 
+const fundo = {
+    spriteX: 389, 
+    spriteY: 0,
+    largura: 276,
+    altura: 204,
+    x: 0,
+    y: 276,
+        back() {
+            contexto.drawImage(
+                sprites,
+                fundo.spriteX, fundo.spriteY,
+                fundo.largura, fundo.altura,
+                fundo.x, fundo.y,
+                fundo.largura, fundo.altura,
+            );
+            contexto.drawImage(
+                sprites,
+                fundo.spriteX, fundo.spriteY,
+                fundo.largura, fundo.altura,
+                fundo.x + fundo.largura + -1, fundo.y,
+                fundo.largura, fundo.altura,);
+        }
+}
+
+const chao = {
+    spriteX: 0, 
+    spriteY: 612,
+    largura: 225,
+    altura: 109,
+    x: 0,
+    y: 371,
+        chao() {
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                chao.x, chao.y,
+                chao.largura, chao.altura,
+            );
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                chao.x + chao.largura + -1, chao.y,
+                chao.largura, chao.altura,
+            );
+        }
+}
+
+contexto.fillStyle = '#70c5ce';
+contexto.fillRect(0,0, canvas.clientWidth, canvas.height)
+
 function loop(){
     flappyBird.desenha();
+    fundo.back();
+    chao.chao();
 
     requestAnimationFrame(loop);
 }
